686b2f2909b05dbe13a938524faf33358d39709e
azilinski <alexanderzilinski@gmail.com>
2014-09-15 16:55:55 -0600

    Created component Library Code
    Created Entity Special Event
    Created Contex class
    Created compent Library

---
+++ Solution1/eResturauntSystem/BLL/eResturauntController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region AdditionalNamespaces
+using eResturauntSystem.Entites;
+using eResturauntSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+namespace eResturauntSystem.BLL
+{
+    [DataObject]
+    public class eResturauntController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<SpecialEvents> SpecialEvent_List()
+        {
+            //interfacing with our Context Class 
+            using (eResturauntContext contex = new eResturauntContext())
+            {
+                return contex.SpecialEvents.ToList();
+            }
+        }
+    }
+}
---
+++ Solution1/eResturauntSystem/DAL/eResturauntContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eResturauntSystem.Entites;
+using System.Data.Entity;
+#endregion
+
+namespace eResturauntSystem.DAL
+{
+    //hookup to Entity Framework via the DbContext base class
+    internal class eResturauntContext:DbContext
+    {
+        //constructor pass to the base class the name value for the 
+        //connetion string to the database found in WebConnectionStrings.config
+        public eResturauntContext() : base("name=EatIn") { }
+
+        //One DbSet is to be created for each entity to be referenced by your 
+        public DbSet<SpecialEvents> SpecialEvents { get; set; }
+        //public DbSet<Reservation> Reservations { get; set; }
+        //public DbSet<Table> Tables { get; set; }
+    }
+}
