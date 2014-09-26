05f9361fe27762f49f73628c8684f1b233f6e226
azilinski <alexanderzilinski@gmail.com>
2014-09-26 10:04:19 -0600

    Fixed some bugs on dropdown

--- Solution1/eResturauntSystem/Entites/Bill.cs
+++ Solution1/eResturauntSystem/Entites/Bill.cs
@@ -1,15 +1,32 @@
-﻿using System;
+﻿using eResturauntSystem.Entites;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace eResturauntSystem.Entites
+namespace eRestaurantSystem.Entities
 {
     public class Bill
     {
         [Key]
-        public int 
+        public int BillID { get; set; }
+        [Required(ErrorMessage = "Bill Date is required.")]
+        public DateTime BillDate { get; set; }
+        public TimeSpan OrderPlaced { get; set; }
+        [Required(ErrorMessage = "Number in party is required.")]
+        public int NumberInParty { get; set; }
+        public bool PaidStatus { get; set; }
+        [Required(ErrorMessage = "Waiter is required.")]
+        public int WaiterID { get; set; }
+        public int TableID { get; set; }
+        public int ReservationID { get; set; }
+        public bool OrderReady { get; set; }
+        [StringLength(50, ErrorMessage = "Comment has a max size of 50 characters")]
+        public string Comment { get; set; }
+
+        //navigation
+        public virtual Waiter Waiter { get; set; }
     }
 }
--- Solution1/eResturauntSystem/Entites/Reservation.cs
+++ Solution1/eResturauntSystem/Entites/Reservation.cs
@@ -12,7 +12,7 @@ namespace eResturauntSystem.Entites
 {
     public class Reservation
     {
-        public int ReservationId { get; set; }
+        public int ReservationID { get; set; }        
         public string CustomerName { get; set; }
         public DateTime ReservationDate { get; set; }
         public int NumberInParty { get; set; }
