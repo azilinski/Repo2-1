8eae771d45284f39082f745d864119562ec5f5e8
azilinski <alexanderzilinski@gmail.com>
2014-11-20 11:22:13 -0700

    Class Demo 3

---
+++ ClassDemos-master/eRestaurantSystem/Entities/Reservation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace eRestaurantSystem.Entities
+{
+    public class Reservation
+    {
+        public const string Booked = "B";
+        public const string Arrived = "A";
+        public const string Complete = "C";
+        public const string NoShow = "N";
+        public const string Cancelled = "X";
+
+        [Key]
+        public int ReservationID { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public int NumberinParty { get; set; }
+        public string ContactPhone { get; set; }
+        public string ReservationStatus { get; set; }
+        public string Eventcode { get; set; }
+
+        //navigation
+        //pointing to a parent one
+        public virtual SpecialEvent SpecialEvent { get; set; }
+        //pointing to a collection many
+        public virtual ICollection<Bill> Bills { get; set; }
+        public virtual ICollection<Table> Tables { get; set; }
+    }
+}
