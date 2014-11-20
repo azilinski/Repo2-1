82dc7f9e771f4d84b962d4f8a64205d679c391bd
azilinski <alexanderzilinski@gmail.com>
2014-11-20 12:01:09 -0700

    x

---
+++ ClassDemos-master/eRestaurantSystem/BLL/Security/UserManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+using eRestaurantSystem.DAL.Security;
+#endregion
+
+namespace eRestaurantSystem.BLL.Security
+{
+    public class UserManager : UserManager<ApplicationUser>
+    {
+        public UserManager()
+            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+        }
+    }
+}
---
+++ ClassDemos-master/eRestaurantSystem/Entities/Security/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+using Microsoft.Owin.Security;
+#endregion
+
+namespace eRestaurantSystem.Entities.Security
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
